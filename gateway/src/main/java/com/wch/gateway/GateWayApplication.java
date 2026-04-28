f4a6a1503789f204c0779d33b816c223d1dde257
huangchaowei <1606162138@qq.com>
2026-04-29 01:58:05 +0800

    build

---
+++ gateway/src/main/java/com/wch/gateway/GateWayApplication.java
@@ -0,0 +1,16 @@
+package com.wch.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: Jie Bugui
+ * @create: 2025-04-22 16:28
+ */
+@SpringBootApplication()
+public class GateWayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayApplication.class, args);
+    }
+}
